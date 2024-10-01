af5fadaa61678d7888c16682648f545bd0812e40
Joyoounjin <dbswls060@gmail.com>
2024-10-01 18:08:30 +0900

    add skill

--- rtanRPG/obj/Debug/net8.0/rtanRPG.AssemblyInfo.cs
+++ rtanRPG/obj/Debug/net8.0/rtanRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c88320adfac0a70c3beaa43f0149dbb5ea8c6ede")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea7886c01fef52edbc97659766afae5907ea3cb")]
 [assembly: System.Reflection.AssemblyProductAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
