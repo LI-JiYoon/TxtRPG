0b042592e1744339b4976c3eddeeab8e31cf8bfc
Zzondy-Unity <abdos230@naver.com>
2024-10-03 16:12:58 +0900

    PPT 추가

--- rtanRPG/obj/Debug/net8.0/rtanRPG.AssemblyInfo.cs
+++ rtanRPG/obj/Debug/net8.0/rtanRPG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87270b5fa075813fbc3fe0d497af1a590f75dce2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdbfabf0ca3bbf008aa50ab93ba9be005c4a7f68")]
 [assembly: System.Reflection.AssemblyProductAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rtanRPG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
